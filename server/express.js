d2a1e48eb5e0c3a22df899d086cf91d4cfd6d124
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-11-03 12:33:06 -0700

    Dynamically register routes

--- server/express.js
+++ server/express.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
+const _ = require('lodash');
+const glob = require('glob');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const initializeApplication = () => {
     console.log('Initializing the express application.');
@@ -17,6 +20,11 @@ const initializeApplication = () => {
     // POST request payloads limited to JSON and no larger than 10mb
     app.use(bodyParser.json({ limit: '10mb' }));
     app.use(morgan('dev', {}));
+
+    // Dynamically registers routes based on route file pattern
+    getRestRouteFilePaths('./**/*.rest.routes.js').forEach((routePath) => {
+        require(path.resolve(routePath))(app);
+    });
 
     return app;
 }
@@ -46,6 +54,15 @@ const startApp = () => {
 
 let server;
 let app = initializeApplication();
+
+function getRestRouteFilePaths(filePattern) {
+    let routeFilePaths = [];
+
+    let files = glob.sync(filePattern);
+    routeFilePaths = _.union(routeFilePaths, files);
+
+    return routeFilePaths;
+}
 
 module.exports = {
     startApp,
